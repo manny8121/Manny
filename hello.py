5ec7eb709ab5b6fa0988653b4f27e99bad56d74a
Diaz-Mendez <352502@mcpsmd.org>
2019-12-02 09:21:10 -0500

    changed number to RandNum

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
+import random
 
-name = input("what is your name?")
-print("hello, ",name + "!")
+randNum = int(random.randrange(0, 50))
+guess = False 
+while guess == False :
+    numberGuess = int(input("Guess the Number!"))
+    if numberGuess == randNum :
+        print(" you got it,don't give up!")
+        checkCor = True 
+    elif numberGuess > randNum :
+        print("go lower boss")
+    elif numberGuess < randNum:
+        print("a bit higher boss")
+if guess==randNum:
+    print("thats whast up!!!")
